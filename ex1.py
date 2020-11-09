f5fdfc33cda323afd2e9a3569245885b1dc326f5
vidyawantstobattle <dhopatevidya@gmail.com>
2020-11-09 23:41:31 +0530

    exercise 1,2 to be added

---
+++ ex1.py
@@ -0,0 +1,7 @@
+print("hello world!")
+print("hello again")
+print("I ike typing this.")
+print("this is fun")
+print("yay! printing!")
+print("I'd much rather you 'not'.")
+print('I "said" do not do this.')
---
+++ ex2.py
@@ -0,0 +1,4 @@
+#code about comments
+#but I already know about comments in python
+print("but I will run it anyway, for consistency's sake") #i hope the author of the book is proud of me
+#or not 
